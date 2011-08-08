27c9e67cfe855eb27bf6225ed27523d64ef7c47b
John Millikin <jmillikin@gmail.com>
2011-08-07 20:29:44 -0700

    Add support for logging test results to plain-text and XML reports.

--- src/Test/Chell.hs
+++ src/Test/Chell.hs
@@ -48,25 +48,28 @@ module Test.Chell
 
 import qualified Control.Exception
 import           Control.Exception (Exception)
-import           Control.Monad (when, forM_)
+import           Control.Monad (foldM, forM_, unless, when)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
-import           Control.Monad.Trans.State (StateT, execStateT, get, put)
 import           Data.Maybe (isJust, isNothing)
+import           Data.IORef (newIORef, readIORef, atomicModifyIORef)
 import qualified Data.Text
 import           Data.Text (Text)
 import qualified Data.Text.IO
+import qualified System.Console.GetOpt as GetOpt
+import           System.Environment (getArgs, getProgName)
 import           System.Exit (exitSuccess, exitFailure)
-import           System.IO (stderr, hPutStr, hPutStrLn)
+import           System.IO (Handle, stderr, hPutStr, hPutStrLn)
+import qualified System.IO as IO
 
 import qualified Language.Haskell.TH as TH
 
 newtype Test = Test { runTest :: IO TestResult }
 
 data TestResult
-	= TestResultSuccess
+	= TestResultPassed
 	| TestResultSkipped
-	| TestResultFailure [Failure]
-	| TestResultError Text
+	| TestResultFailed [Failure]
+	| TestResultAborted Text
 
 data Failure = Failure (Maybe Location) Text
 
@@ -143,12 +146,12 @@ assertions testm = Test io where
 	io = do
 		tried <- Control.Exception.try (unTestM testm [])
 		return $ case tried of
-			Left exc -> TestResultError (errorExc exc)
-			Right (_, []) -> TestResultSuccess
-			Right (_, fs) -> TestResultFailure fs
+			Left exc -> TestResultAborted (errorExc exc)
+			Right (_, []) -> TestResultPassed
+			Right (_, fs) -> TestResultFailed fs
 	
 	errorExc :: Control.Exception.SomeException -> Text
-	errorExc exc = Data.Text.pack ("Test aborted with exception: " ++ show exc)
+	errorExc exc = Data.Text.pack ("Test aborted due to exception: " ++ show exc)
 
 addFailure :: Maybe TH.Loc -> Bool -> Text -> Assertions
 addFailure maybe_loc fatal msg = TestM $ \fs -> do
@@ -199,65 +202,242 @@ expect = do
 	let qloc = liftLoc loc
 	[| assertAt $qloc False |]
 
+data Option
+	= OptionHelp
+	| OptionVerbose
+	| OptionXmlReport FilePath
+	| OptionLog FilePath
+	deriving (Show, Eq)
+
+optionInfo :: [GetOpt.OptDescr Option]
+optionInfo =
+	[ GetOpt.Option ['h'] ["help"]
+	  (GetOpt.NoArg OptionHelp)
+	  "show this help"
+	
+	, GetOpt.Option ['v'] ["verbose"]
+	  (GetOpt.NoArg OptionVerbose)
+	  "print more output"
+	
+	, GetOpt.Option [] ["xml-report"]
+	  (GetOpt.ReqArg OptionXmlReport "PATH")
+	  "write a parsable report to a file, in XML"
+	
+	, GetOpt.Option [] ["log"]
+	  (GetOpt.ReqArg OptionLog "PATH")
+	  "write a full log (always max verbosity) to a file path"
+	]
+
+usage :: String -> String
+usage name = "Usage: " ++ name ++ " [OPTION...]"
+
 defaultMain :: [Suite] -> IO ()
 defaultMain suites = do
-	let state = (0, 0, 0, 0)
+	args <- getArgs
+	let (options, _, optionErrors) = GetOpt.getOpt GetOpt.Permute optionInfo args
+	unless (null optionErrors) $ do
+		name <- getProgName
+		hPutStrLn stderr (concat optionErrors)
+		hPutStrLn stderr (GetOpt.usageInfo (usage name) optionInfo)
+		exitFailure
+	
+	when (OptionHelp `elem` options) $ do
+		name <- getProgName
+		putStrLn (GetOpt.usageInfo (usage name) optionInfo)
+		exitSuccess
+	
 	let tests = concatMap suiteTests suites
-	endState <- execStateT (mapM_ recordTest tests) state
-	let (passed, skipped, failed, errors) = endState
-	let allPassed = case (failed, errors) of
-		(0, 0) -> True
-		_      -> False
-	if allPassed
-		then hPutStr stderr "PASS: "
-		else hPutStr stderr "FAIL: "
-	
-	let putNum comma n what = hPutStr stderr $ if n == 1
-		then comma ++ "1 test " ++ what
-		else comma ++ show n ++ " tests " ++ what
-	
-	let total = sum [passed, skipped, failed, errors]
-	putNum "" total "run"
-	when (passed > 0) (putNum ", " passed "passed")
-	when (skipped > 0) (putNum ", " skipped "skipped")
-	when (failed > 0) (putNum ", " failed "failed")
-	when (errors > 0) (putNum ", " errors "aborted")
-	hPutStr stderr "\n"
+	allPassed <- withReports options $ do
+		ReportsM (mapM_ reportStarted)
+		allPassed <- foldM (\good t -> do
+			thisGood <- reportTest t
+			return (good && thisGood)) True tests
+		ReportsM (mapM_ reportFinished)
+		return allPassed
 	
 	if allPassed
 		then exitSuccess
 		else exitFailure
 
-recordTest :: (Text, Test) -> StateT (Integer, Integer, Integer, Integer) IO ()
-recordTest (name, t) = do
-	result <- liftIO (runTest t)
-	(passed, skipped, failed, errors) <- get
-	newState <- liftIO $ case result of
-		TestResultSuccess -> return (passed + 1, skipped, failed, errors)
-		TestResultSkipped -> return (passed, skipped + 1, failed, errors)
-		TestResultFailure fs -> do
-			hPutStr stderr "FAIL "
-			Data.Text.IO.hPutStrLn stderr name
+data Report = Report
+	{ reportStarted :: IO ()
+	, reportPassed :: Text -> IO ()
+	, reportSkipped :: Text -> IO ()
+	, reportFailed :: Text -> [Failure] -> IO ()
+	, reportAborted :: Text -> Text -> IO ()
+	, reportFinished :: IO ()
+	}
+
+xmlReport :: Handle -> Report
+xmlReport h = Report
+	{ reportStarted = do
+		hPutStrLn h "<?xml version=\"1.0\" encoding=\"utf8\"?>"
+		hPutStrLn h "<report xmlns='urn:john-millikin:chell:report:1'>"
+	, reportPassed = \name -> do
+		hPutStr h "\t<test-run test='"
+		hPutStr h (escapeXML name)
+		hPutStrLn h "' result='passed'/>"
+	, reportSkipped = \name -> do
+		hPutStr h "\t<test-run test='"
+		hPutStr h (escapeXML name)
+		hPutStrLn h "' result='skipped'/>"
+	, reportFailed = \name fs -> do
+		hPutStr h "\t<test-run test='"
+		hPutStr h (escapeXML name)
+		hPutStrLn h "' result='failed'>"
+		forM_ fs $ \(Failure loc msg) -> do
+			hPutStr h "\t\t<failure message='"
+			hPutStr h (escapeXML msg)
+			case loc of
+				Just loc' -> do
+					hPutStrLn h "'>"
+					hPutStr h "\t\t\t<location module='"
+					hPutStr h (escapeXML (locationModule loc'))
+					hPutStr h "' file='"
+					hPutStr h (escapeXML (locationFile loc'))
+					hPutStr h "' line='"
+					hPutStr h (show (locationLine loc'))
+					hPutStrLn h "'/>"
+					hPutStrLn h "\t\t</failure>"
+				Nothing -> hPutStrLn h "'/>"
+		hPutStrLn h "\t</test-run>"
+	, reportAborted = \name msg -> do
+		hPutStr h "\t<test-run test='"
+		hPutStr h (escapeXML name)
+		hPutStrLn h "' result='aborted'>"
+		hPutStr h "\t\t<abortion message='"
+		hPutStr h (escapeXML msg)
+		hPutStrLn h "'/>"
+		hPutStrLn h "\t</test-run>"
+	, reportFinished = do
+		hPutStrLn h "</report>"
+	}
+
+escapeXML :: Text -> String
+escapeXML = concatMap (\c -> case c of
+	'&' -> "&amp;"
+	'<' -> "&lt;"
+	'>' -> "&gt;"
+	'"' -> "&quot;"
+	'\'' -> "&apos;"
+	_ -> [c]) . Data.Text.unpack
+
+textReport :: Bool -> Handle -> IO Report
+textReport verbose h = do
+	countPassed <- newIORef (0 :: Integer)
+	countSkipped <- newIORef (0 :: Integer)
+	countFailed <- newIORef (0 :: Integer)
+	countAborted <- newIORef (0 :: Integer)
+	
+	let incRef ref = atomicModifyIORef ref (\a -> (a + 1, ()))
+	
+	return (Report
+		{ reportStarted = return ()
+		, reportPassed = \name -> do
+			when verbose $ do
+				hPutStr h "PASS "
+				Data.Text.IO.hPutStrLn h name
+			incRef countPassed
+		, reportSkipped = \name -> do
+			when verbose $ do
+				hPutStr h "SKIP "
+				Data.Text.IO.hPutStrLn h name
+			incRef countSkipped
+		, reportFailed = \name fs -> do
+			hPutStr h "FAIL "
+			Data.Text.IO.hPutStrLn h name
 			forM_ fs $ \(Failure loc msg) -> do
 				case loc of
 					Just loc' -> do
-						hPutStr stderr "\t"
-						Data.Text.IO.hPutStr stderr (locationFile loc')
-						hPutStr stderr ":"
-						hPutStrLn stderr (show (locationLine loc'))
+						hPutStr h "\t"
+						Data.Text.IO.hPutStr h (locationFile loc')
+						hPutStr h ":"
+						hPutStrLn h (show (locationLine loc'))
 					Nothing -> return ()
-				hPutStr stderr "\t\t"
-				Data.Text.IO.hPutStrLn stderr msg
-				hPutStr stderr "\n"
-			return (passed, skipped, failed + 1, errors)
-		TestResultError err -> do
-			hPutStr stderr "ERROR "
-			Data.Text.IO.hPutStrLn stderr name
-			hPutStr stderr "\t"
-			Data.Text.IO.hPutStrLn stderr err
-			hPutStr stderr "\n"
-			return (passed, skipped, failed, errors + 1)
-	put newState
+				hPutStr h "\t\t"
+				Data.Text.IO.hPutStrLn h msg
+				hPutStr h "\n"
+			incRef countFailed
+		, reportAborted = \name msg -> do
+			hPutStr h "ABRT "
+			Data.Text.IO.hPutStrLn h name
+			hPutStr h "\t"
+			Data.Text.IO.hPutStrLn h msg
+			hPutStr h "\n"
+			incRef countAborted
+		, reportFinished = do
+			n_passed <- readIORef countPassed
+			n_skipped <- readIORef countSkipped
+			n_failed <- readIORef countFailed
+			n_aborted <- readIORef countAborted
+			if n_failed == 0 && n_aborted == 0
+				then hPutStr h "PASS: "
+				else hPutStr h "FAIL: "
+			let putNum comma n what = hPutStr h $ if n == 1
+				then comma ++ "1 test " ++ what
+				else comma ++ show n ++ " tests " ++ what
+			
+			let total = sum [n_passed, n_skipped, n_failed, n_aborted]
+			putNum "" total "run"
+			when (n_passed > 0) (putNum ", " n_passed "passed")
+			when (n_skipped > 0) (putNum ", " n_skipped "skipped")
+			when (n_failed > 0) (putNum ", " n_failed "failed")
+			when (n_aborted > 0) (putNum ", " n_aborted "aborted")
+			hPutStr h "\n"
+		})
+
+withReports :: [Option] -> ReportsM a -> IO a
+withReports opts reportsm = do
+	let loop [] reports = runReportsM reportsm (reverse reports)
+	    loop (o:os) reports = case o of
+	    	OptionXmlReport path -> IO.withBinaryFile path IO.WriteMode
+	    		(\h -> loop os (xmlReport h : reports))
+	    	OptionLog path -> IO.withBinaryFile path IO.WriteMode
+	    		(\h -> textReport True h >>= \r -> loop os (r : reports))
+	    	_ -> loop os reports
+	
+	console <- textReport (OptionVerbose `elem` opts) stderr
+	loop opts [console]
+
+newtype ReportsM a = ReportsM { runReportsM :: [Report] -> IO a }
+
+instance Monad ReportsM where
+	return x = ReportsM (\_ -> return x)
+	m >>= f = ReportsM (\reports -> do
+		x <- runReportsM m reports
+		runReportsM (f x) reports)
+
+instance MonadIO ReportsM where
+	liftIO io = ReportsM (\_ -> io)
+
+reportTest :: (Text, Test) -> ReportsM Bool
+reportTest (name, t) = do
+	result <- liftIO (runTest t)
+	case result of
+		TestResultPassed -> do
+			passed name
+			return True
+		TestResultSkipped -> do
+			skipped name
+			return True
+		TestResultFailed fs -> do
+			failed name fs
+			return False
+		TestResultAborted msg -> do
+			aborted name msg
+			return False
+
+passed :: Text -> ReportsM ()
+passed name = ReportsM (mapM_ (\r -> reportPassed r name))
+
+skipped :: Text -> ReportsM ()
+skipped name = ReportsM (mapM_ (\r -> reportSkipped r name))
+
+failed :: Text -> [Failure] -> ReportsM ()
+failed name fs = ReportsM (mapM_ (\r -> reportFailed r name fs))
+
+aborted :: Text -> Text -> ReportsM ()
+aborted name msg = ReportsM (mapM_ (\r -> reportAborted r name msg))
 
 pure :: Bool -> String -> Assertion
 pure True _ = Assertion (return Nothing)
