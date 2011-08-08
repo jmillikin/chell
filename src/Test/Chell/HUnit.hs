19870a9558cb993bd7f50f3e061b810aa82b2e87
John Millikin <jmillikin@gmail.com>
2011-08-08 15:41:15 -0700

    Initial import of chell-hunit, an accessory library to Chell for running HUnit tests.

---
+++ src/Test/Chell/HUnit.hs
@@ -0,0 +1,34 @@
+module Test.Chell.HUnit
+	( hunit
+	) where
+
+import           Data.Text (Text, pack)
+
+import qualified Test.Chell as Chell
+import           Test.HUnit.Lang (Assertion, performTestCase)
+
+-- | Convert a sequence of HUnit assertions (embedded in IO) to a Chell
+-- 'Chell.Suite'.
+--
+-- @
+-- import Test.Chell
+-- import Test.Chell.HUnit
+-- import Test.HUnit
+--
+-- tests :: [Suite]
+-- tests =
+--     [ suite \"foo\"
+--         [ hunit \"bar\" $ do
+--             1 + 2 \@?= 3
+--         ]
+--     ]
+-- @
+hunit :: Text -> Assertion -> Chell.Suite
+hunit name io = Chell.test name (Chell.Test chell_io) where
+	chell_io = do
+		result <- performTestCase io
+		return $ case result of
+			Nothing -> Chell.TestPassed
+			Just err -> parseError err
+	parseError (True, msg) = Chell.TestFailed [Chell.Failure Nothing (pack msg)]
+	parseError (False, msg) = Chell.TestAborted (pack msg)
