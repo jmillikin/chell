8407cdca7b45f3f49d40ff29b3389177375b919a
John Millikin <jmillikin@gmail.com>
2011-08-07 22:07:53 -0700

    Minor cleanups, and add some Haddock documentation.

--- src/Test/Chell.hs
+++ src/Test/Chell.hs
@@ -4,35 +4,37 @@
 module Test.Chell
 	(
 	
-	-- * Tests
-	  Test (..)
+	-- * Main
+	  defaultMain
+	
+	-- ** Tests
+	, Test (..)
 	, TestResult (..)
 	, Failure (..)
 	, Location (..)
 	, skip
 	, skipIf
 	
-	-- * Suites
+	-- ** Suites
 	, Suite
 	, suite
 	, test
-	, suiteName
 	, suiteTests
 	
-	-- * Main
-	, defaultMain
-	
-	-- * basic
-	, Assertion
+	-- * Basic testing library
+	-- $doc-basic-testing
+	, Assertion (..)
+	, AssertionResult (..)
 	, IsAssertion
 	, Assertions
+	, TestM
 	, assertions
 	, assert
 	, expect
 	, Test.Chell.fail
 	, trace
 	
-	-- ** assertions
+	-- ** Assertions
 	, equal
 	, notEqual
 	, equalWithin
@@ -48,7 +50,7 @@ module Test.Chell
 
 import qualified Control.Exception
 import           Control.Exception (Exception)
-import           Control.Monad (foldM, forM_, unless, when)
+import           Control.Monad (foldM, forM_, liftM, unless, when)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Data.Char (ord)
 import           Data.List (intercalate)
@@ -66,13 +68,16 @@ import           Text.Printf (printf)
 
 import qualified Language.Haskell.TH as TH
 
-newtype Test = Test { runTest :: IO TestResult }
+newtype Test = Test (IO TestResult)
+
+runTest :: Test -> IO TestResult
+runTest (Test io) = io
 
 data TestResult
-	= TestResultPassed
-	| TestResultSkipped
-	| TestResultFailed [Failure]
-	| TestResultAborted Text
+	= TestPassed
+	| TestSkipped
+	| TestFailed [Failure]
+	| TestAborted Text
 
 data Failure = Failure (Maybe Location) Text
 
@@ -82,16 +87,36 @@ data Location = Location
 	, locationLine :: Integer
 	}
 
+-- | A test which is always skipped. Use this to avoid commenting out tests
+-- which are currently broken, or do not work on the current platform.
+--
+-- @
+--tests = 'suite' \"tests\"
+--    [ 'test' \"windows-specific\" $ if onWindows
+--        then test_WindowsSpecific
+--        else 'skip'
+--    ]
+-- @
+--
 skip :: Test
-skip = Test (return TestResultSkipped)
+skip = Test (return TestSkipped)
 
+-- | Potentially skip a test, depending on the result of a runtime check.
+--
+-- @
+--tests = 'suite' \"tests\"
+--    [ 'test' \"ping-google\" ('skipIf' noNetwork test_PingGoogle)
+--    ]
+-- @
 skipIf :: IO Bool -> Test -> Test
 skipIf p t = Test $ do
 	skipThis <- p
 	if skipThis
-		then return TestResultSkipped
+		then return TestSkipped
 		else runTest t
 
+-- | Running a 'Test' requires it to be contained in a 'Suite'. This gives
+-- the test a name, so users know which test failed.
 data Suite = Suite Text [Suite]
            | SuiteTest Text Test
 
@@ -105,6 +130,9 @@ suiteName :: Suite -> Text
 suiteName (Suite name _) = name
 suiteName (SuiteTest name _) = name
 
+-- | The full list of 'Test's contained within this 'Suite'. Each 'Test'
+-- is returned along with its full name, which includes the name of its
+-- parent 'Suite's.
 suiteTests :: Suite -> [(Text, Test)]
 suiteTests = loop "" where
 	loop prefix s = let
@@ -115,7 +143,17 @@ suiteTests = loop "" where
 			Suite _ suites -> concatMap (loop name) suites
 			SuiteTest _ t -> [(name, t)]
 
-newtype Assertion = Assertion (IO (Maybe Text))
+-- $doc-basic-testing
+--
+-- This library includes a few basic JUnit-style assertions, for use in
+-- simple test suites where depending on a separate test framework is too
+-- much trouble.
+
+newtype Assertion = Assertion (IO AssertionResult)
+
+data AssertionResult
+	= AssertionPassed
+	| AssertionFailed Text
 
 class IsAssertion a where
 	toAssertion :: a -> Assertion
@@ -125,11 +163,14 @@ instance IsAssertion Assertion where
 
 instance IsAssertion Bool where
 	toAssertion x = Assertion (return (if x
-		then Nothing
-		else Just "boolean assertion failed"))
+		then AssertionPassed
+		else AssertionFailed "boolean assertion failed"))
 
 type Assertions = TestM ()
 newtype TestM a = TestM { unTestM :: [Failure] -> IO (Maybe a, [Failure]) }
+
+instance Functor TestM where
+	fmap = liftM
 
 instance Monad TestM where
 	return x = TestM (\s -> return (Just x, s))
@@ -144,14 +185,22 @@ instance MonadIO TestM where
 		x <- io
 		return (Just x, s))
 
+-- | Convert a sequence of pass/fail assertions into a runnable test.
+--
+-- @
+-- test_Equality :: Test
+-- test_Equality = assertions $ do
+--     $assert (1 == 1)
+--     $assert (equal 1 1)
+-- @
 assertions :: Assertions -> Test
 assertions testm = Test io where
 	io = do
 		tried <- Control.Exception.try (unTestM testm [])
 		return $ case tried of
-			Left exc -> TestResultAborted (errorExc exc)
-			Right (_, []) -> TestResultPassed
-			Right (_, fs) -> TestResultFailed fs
+			Left exc -> TestAborted (errorExc exc)
+			Right (_, []) -> TestPassed
+			Right (_, fs) -> TestFailed fs
 	
 	errorExc :: Control.Exception.SomeException -> Text
 	errorExc exc = Data.Text.pack ("Test aborted due to exception: " ++ show exc)
@@ -168,12 +217,22 @@ addFailure maybe_loc fatal msg = TestM $ \fs -> do
 	return ( if fatal then Nothing else Just ()
 	       , Failure loc msg : fs)
 
+-- | Cause a test to immediately fail, with a message.
+--
+-- 'fail' is a Template Haskell macro, to retain the source-file location
+-- from which it was used. Its effective type is:
+--
+-- @
+-- $fail :: 'Text' -> 'Assertions'
+-- @
 fail :: TH.Q TH.Exp -- :: Text -> Assertions
 fail = do
 	loc <- TH.location
 	let qloc = liftLoc loc
 	[| addFailure (Just $qloc) True |]
 
+-- | Print a message from within a test. This is just a helper for debugging,
+-- so you don't have to import @Debug.Trace@.
 trace :: Text -> Assertions
 trace msg = liftIO (Data.Text.IO.putStrLn msg)
 
@@ -190,15 +249,33 @@ assertAt loc fatal assertion = do
 	let Assertion io = toAssertion assertion
 	result <- liftIO io
 	case result of
-		Nothing -> return ()
-		Just err -> addFailure (Just loc) fatal err
+		AssertionPassed -> return ()
+		AssertionFailed err -> addFailure (Just loc) fatal err
 
+-- | Run an 'Assertion'. If the assertion fails, the test will immediately
+-- fail.
+--
+-- 'assert' is a Template Haskell macro, to retain the source-file location
+-- from which it was used. Its effective type is:
+--
+-- @
+-- $assert :: 'IsAssertion' assertion => assertion -> 'Assertions'
+-- @
 assert :: TH.Q TH.Exp -- :: IsAssertion assertion => assertion -> Assertions
 assert = do
 	loc <- TH.location
 	let qloc = liftLoc loc
 	[| assertAt $qloc True |]
 
+-- | Run an 'Assertion'. If the assertion fails, the test will continue to
+-- run until it finishes (or until an 'assert' fails).
+--
+-- 'expect' is a Template Haskell macro, to retain the source-file location
+-- from which it was used. Its effective type is:
+--
+-- @
+-- $expect :: 'IsAssertion' assertion => assertion -> 'Assertions'
+-- @
 expect :: TH.Q TH.Exp -- :: IsAssertion assertion => assertion -> Assertions
 expect = do
 	loc <- TH.location
@@ -239,6 +316,8 @@ optionInfo =
 usage :: String -> String
 usage name = "Usage: " ++ name ++ " [OPTION...]"
 
+-- | A simple default main function, which runs a list of tests and logs
+-- statistics to stderr.
 defaultMain :: [Suite] -> IO ()
 defaultMain suites = do
 	args <- getArgs
@@ -483,16 +562,16 @@ reportTest :: (Text, Test) -> ReportsM Bool
 reportTest (name, t) = do
 	result <- liftIO (runTest t)
 	case result of
-		TestResultPassed -> do
+		TestPassed -> do
 			passed name
 			return True
-		TestResultSkipped -> do
+		TestSkipped -> do
 			skipped name
 			return True
-		TestResultFailed fs -> do
+		TestFailed fs -> do
 			failed name fs
 			return False
-		TestResultAborted msg -> do
+		TestAborted msg -> do
 			aborted name msg
 			return False
 
@@ -509,52 +588,70 @@ aborted :: Text -> Text -> ReportsM ()
 aborted name msg = ReportsM (mapM_ (\r -> reportAborted r name msg))
 
 pure :: Bool -> String -> Assertion
-pure True _ = Assertion (return Nothing)
-pure False err = Assertion (return (Just (Data.Text.pack err)))
+pure True _ = Assertion (return AssertionPassed)
+pure False err = Assertion (return (AssertionFailed (Data.Text.pack err)))
 
+-- | Assert that two values are equal.
 equal :: (Show a, Eq a) => a -> a -> Assertion
 equal x y = pure (x == y) ("equal: " ++ show x ++ " is not equal to " ++ show y)
 
+-- | Assert that two values are not equal.
 notEqual :: (Eq a, Show a) => a -> a -> Assertion
 notEqual x y = pure (x /= y) ("notEqual: " ++ show x ++ " is equal to " ++ show y)
 
-equalWithin :: (Real a, Show a) => a -> a -> a -> Assertion
+-- | Assert that two values are within some delta of each other.
+equalWithin :: (Real a, Show a) => a -> a
+                                -> a -- ^ delta
+                                -> Assertion
 equalWithin x y delta = pure
 	((x - delta <= y) && (x + delta >= y))
 	("equalWithin: " ++ show x ++ " is not within " ++ show delta ++ " of " ++ show y)
 
+-- | Assert that some value is @Just@.
 just :: Maybe a -> Assertion
 just x = pure (isJust x) ("just: received Nothing")
 
+-- | Assert that some value is @Nothing@.
 nothing :: Maybe a -> Assertion
 nothing x = pure (isNothing x) ("nothing: received Just")
 
+-- | Assert that some computation throws an exception matching the provided
+-- predicate. This is mostly useful for exception types which do not have an
+-- instance for @Eq@, such as @'Control.Exception.ErrorCall'@.
 throws :: Exception err => (err -> Bool) -> IO a -> Assertion
 throws p io = Assertion (do
 	either_exc <- Control.Exception.try io
 	return (case either_exc of
 		Left exc -> if p exc
-			then Nothing
-			else Just (Data.Text.pack ("throws: exception " ++ show exc ++ " did not match predicate"))
-		Right _ -> Just (Data.Text.pack ("throws: no exception thrown"))))
+			then AssertionPassed
+			else AssertionFailed (Data.Text.pack ("throws: exception " ++ show exc ++ " did not match predicate"))
+		Right _ -> AssertionFailed (Data.Text.pack ("throws: no exception thrown"))))
 
+-- | Assert that some computation throws an exception equal to the given
+-- exception. This is better than just checking that the correct type was
+-- thrown, because the test can also verify the exception contains the correct
+-- information.
 throwsEq :: (Eq err, Exception err, Show err) => err -> IO a -> Assertion
 throwsEq expected io = Assertion (do
 	either_exc <- Control.Exception.try io
 	return (case either_exc of
 		Left exc -> if exc == expected
-			then Nothing
-			else Just (Data.Text.pack ("throwsEq: exception " ++ show exc ++ " is not equal to " ++ show expected))
-		Right _ -> Nothing))
+			then AssertionPassed
+			else AssertionFailed (Data.Text.pack ("throwsEq: exception " ++ show exc ++ " is not equal to " ++ show expected))
+		Right _ -> AssertionFailed (Data.Text.pack ("throwsEq: no exception thrown"))))
 
+-- | Assert a value is greater than another.
 greater :: (Ord a, Show a) => a -> a -> Assertion
 greater x y = pure (x > y) ("greater: " ++ show x ++ " is not greater than " ++ show y)
 
+-- | Assert a value is greater than or equal to another.
 greaterEqual :: (Ord a, Show a) => a -> a -> Assertion
 greaterEqual x y = pure (x > y) ("greaterEqual: " ++ show x ++ " is not greater than or equal to " ++ show y)
 
+-- | Assert a value is less than another.
 lesser :: (Ord a, Show a) => a -> a -> Assertion
 lesser x y = pure (x < y) ("lesser: " ++ show x ++ " is not less than " ++ show y)
 
+-- | Assert a value is less than or equal to another.
 lesserEqual :: (Ord a, Show a) => a -> a -> Assertion
 lesserEqual x y = pure (x <= y) ("lesserEqual: " ++ show x ++ " is not less than or equal to " ++ show y)
