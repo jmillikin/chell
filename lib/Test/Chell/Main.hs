19792ca8d5d6c1b1795d12b9b12a5ef383397051
John Millikin <jmillikin@gmail.com>
2012-05-21 20:10:24 -0700

    Use the "options" package for parsing command-line options to 'defaultMain'.

--- lib/Test/Chell/Main.hs
+++ lib/Test/Chell/Main.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Test.Chell.Main
 	( defaultMain
 	) where
 
-import           Control.Monad (forM, forM_, unless, when)
+import           Control.Monad (forM, forM_, when)
 import           Control.Monad.Trans.Class (lift)
 import qualified Control.Monad.Trans.State as State
 import qualified Control.Monad.Trans.Writer as Writer
@@ -14,129 +15,85 @@ import qualified Data.Text
 import           Data.Text (Text, pack)
 import qualified Data.Text.Encoding
 import qualified Data.Text.IO
-import qualified System.Console.GetOpt as GetOpt
-import           System.Environment (getArgs, getProgName)
 import           System.Exit (exitSuccess, exitFailure)
 import qualified System.IO as IO
 import           System.Random (randomIO)
 import           Text.Printf (printf)
 
+import qualified Filesystem.Path.CurrentOS as Path
+import           Options
+
 import           Test.Chell.Types
 
-data Option
-	= OptionHelp
-	| OptionVerbose
-	| OptionXmlReport FilePath
-	| OptionJsonReport FilePath
-	| OptionTextReport FilePath
-	| OptionSeed Int
-	| OptionTimeout Int
-	deriving (Show, Eq)
-
-optionInfo :: [GetOpt.OptDescr Option]
-optionInfo =
-	[ GetOpt.Option ['h'] ["help"]
-	  (GetOpt.NoArg OptionHelp)
-	  "show this help"
+defineOptions "MainOptions" $ do
+	option "optVerbose" (\o -> o
+		{ optionShortFlags = ['v']
+		, optionLongFlags = ["verbose"]
+		, optionType = optionTypeBool
+		, optionDefault = "false"
+		, optionDescription = "Print more output"
+		})
 	
-	, GetOpt.Option ['v'] ["verbose"]
-	  (GetOpt.NoArg OptionVerbose)
-	  "print more output"
+	pathOption "optXmlReport" "xml-report" ""
+		"Write a parsable report to a given path, in XML."
+	pathOption "optJsonReport" "json-report" ""
+		"Write a parsable report to a given path, in JSON."
+	pathOption "optTextReport" "text-report" ""
+		"Write a human-readable report to a given path."
 	
-	, GetOpt.Option [] ["xml-report"]
-	  (GetOpt.ReqArg OptionXmlReport "PATH")
-	  "write a parsable report to a file, in XML"
+	option "optSeed" (\o -> o
+		{ optionLongFlags = ["seed"]
+		, optionType = optionTypeMaybe optionTypeInt
+		, optionDefault = ""
+		, optionDescription = "The seed used for random numbers in (for example) quickcheck."
+		})
 	
-	, GetOpt.Option [] ["json-report"]
-	  (GetOpt.ReqArg OptionJsonReport "PATH")
-	  "write a parsable report to a file, in JSON"
-	
-	, GetOpt.Option [] ["text-report"]
-	  (GetOpt.ReqArg OptionTextReport "PATH")
-	  "write a human-readable report"
-	
-	, GetOpt.Option [] ["seed"]
-	  (GetOpt.ReqArg (\s -> case parseInt s of
-	   	Just x -> OptionSeed x
-	   	Nothing -> error ("Failed to parse --seed value " ++ show s)) "SEED")
-	  "the seed used for random numbers in (for example) quickcheck"
-	
-	, GetOpt.Option [] ["timeout"]
-	  (GetOpt.ReqArg (\s -> case parseTimeout s of
-	  	Just x -> OptionTimeout x
-	  	Nothing -> error ("Failed to parse --timeout value " ++ show s)) "TIMEOUT")
-	  "the maximum duration of a test, in milliseconds"
-	
-	]
-
-parseInt :: String -> Maybe Int
-parseInt s = case [x | (x, "") <- reads s] of
-	[x] -> Just x
-	_ -> Nothing
-
-parseTimeout :: String -> Maybe Int
-parseTimeout s = do
-	msec <- parseInt s
-	if (toInteger msec) * 1000 > toInteger (maxBound :: Int)
-		then Nothing
-		else return msec
-
-getSeedOpt :: [Option] -> Maybe Int
-getSeedOpt [] = Nothing
-getSeedOpt ((OptionSeed s) : _) = Just s
-getSeedOpt (_:os) = getSeedOpt os
-
-getTimeoutOpt :: [Option] -> Maybe Int
-getTimeoutOpt [] = Nothing
-getTimeoutOpt ((OptionTimeout s) : _) = Just s
-getTimeoutOpt (_:os) = getTimeoutOpt os
-
-usage :: String -> String
-usage name = "Usage: " ++ name ++ " [OPTION...]"
+	option "optTimeout" (\o -> o
+		{ optionLongFlags = ["timeout"]
+		, optionType = optionTypeMaybe optionTypeInt
+		, optionDefault = ""
+		, optionDescription = "The maximum duration of a test, in milliseconds."
+		})
 
 -- | A simple default main function, which runs a list of tests and logs
 -- statistics to stderr.
 defaultMain :: [Suite] -> IO ()
-defaultMain suites = do
-	args <- getArgs
-	let (options, filters, optionErrors) = GetOpt.getOpt GetOpt.Permute optionInfo args
-	unless (null optionErrors) $ do
-		name <- getProgName
-		IO.hPutStrLn IO.stderr (concat optionErrors)
-		IO.hPutStrLn IO.stderr (GetOpt.usageInfo (usage name) optionInfo)
-		exitFailure
-	
-	when (OptionHelp `elem` options) $ do
-		name <- getProgName
-		putStrLn (GetOpt.usageInfo (usage name) optionInfo)
-		exitSuccess
-	
-	let allTests = concatMap suiteTests suites
-	let tests = if null filters
-		then allTests
-		else filter (matchesFilter filters) allTests
-	
-	seed <- case getSeedOpt options of
+defaultMain suites = runCommand $ \opts args -> do
+	-- validate/sanitize test options
+	seed <- case optSeed opts of
 		Just s -> return s
 		Nothing -> randomIO
-	
+	timeout <- case optTimeout opts of
+		Nothing -> return Nothing
+		Just t -> if toInteger t * 1000 > toInteger (maxBound :: Int)
+			then do
+				IO.hPutStrLn IO.stderr "Test.Chell.defaultMain: Ignoring --timeout because it is too large."
+				return Nothing
+			else return (Just t)
 	let testOptions = defaultTestOptions
 		{ testOptionSeed = seed
-		, testOptionTimeout = getTimeoutOpt options
+		, testOptionTimeout = timeout
 		}
 	
-	let verbose = elem OptionVerbose options
+	-- find which tests to run
+	let allTests = concatMap suiteTests suites
+	let tests = if null args
+		then allTests
+		else filter (matchesFilter args) allTests
+	
+	-- run tests
 	results <- forM tests $ \t -> do
 		result <- runTest t testOptions
-		printResult verbose t result
+		printResult (optVerbose opts) t result
 		return (t, result)
 	
-	let reports = getReports options
+	-- generate reports
+	let reports = getReports opts
 	forM_ reports $ \(path, fmt, toText) ->
 		IO.withBinaryFile path IO.WriteMode $ \h -> do
 			let text = toText results
 			let bytes = Data.Text.Encoding.encodeUtf8 text
-			when verbose $ do
+			when (optVerbose opts) $ do
 				putStrLn ("Writing " ++ fmt ++ " report to " ++ show path)
 			Data.ByteString.hPut h bytes
 	
@@ -196,12 +153,17 @@ printResult verbose t result = case result of
 
 type Report = [(Test, TestResult)] -> Text
 
-getReports :: [Option] -> [(FilePath, String, Report)]
-getReports = concatMap step where
-	step (OptionXmlReport path) = [(path, "XML", xmlReport)]
-	step (OptionJsonReport path) = [(path, "JSON", jsonReport)]
-	step (OptionTextReport path) = [(path, "text", textReport)]
-	step _  = []
+getReports :: MainOptions -> [(String, String, Report)]
+getReports opts = concat [xml, json, text] where
+	xml = case optXmlReport opts of
+		p | Path.null p -> []
+		p -> [(Path.encodeString p, "XML", xmlReport)]
+	json = case optJsonReport opts of
+		p | Path.null p -> []
+		p -> [(Path.encodeString p, "JSON", jsonReport)]
+	text = case optTextReport opts of
+		p | Path.null p -> []
+		p -> [(Path.encodeString p, "text", textReport)]
 
 jsonReport :: [(Test, TestResult)] -> Text
 jsonReport results = Writer.execWriter writer where
