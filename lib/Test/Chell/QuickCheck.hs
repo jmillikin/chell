6746f23586955a87419ca35bba33081dadc42adc
John Millikin <jmillikin@gmail.com>
2012-05-23 19:44:41 -0700

    Update for chell_0.3 . Bump version to 0.2 .

--- lib/Test/Chell/QuickCheck.hs
+++ lib/Test/Chell/QuickCheck.hs
@@ -1,12 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Test.Chell.QuickCheck
 	( property
 	) where
 
-import           Data.Text (Text, pack)
-
 import           System.Random (mkStdGen)
+
 import qualified Test.Chell as Chell
 import qualified Test.QuickCheck as QuickCheck
 import qualified Test.QuickCheck.Gen as Gen
@@ -14,58 +11,47 @@ import qualified Test.QuickCheck.State as State
 import qualified Test.QuickCheck.Test as Test
 import qualified Test.QuickCheck.Text as Text
 
--- | Convert a QuickCheck property to a Chell 'Chell.Suite'.
+-- | Convert a QuickCheck property to a Chell 'Chell.Test'.
 --
 -- @
 --import Test.Chell
 --import Test.Chell.QuickCheck
 --import Test.QuickCheck hiding (property)
 --
---tests :: [Suite]
---tests =
---    [ suite \"foo\"
---        [ property \"bar\" (\xs -> not (null xs) ==> length xs > 0)
---        ]
---    ]
+--test_NullLength :: Test
+--test_NullLength = property \"null-length\"
+--    (\xs -> not (null xs) ==> length xs > 0)
 -- @
-property :: QuickCheck.Testable prop => Text -> prop -> Chell.Suite
-property name prop = Chell.test (Chell.Test name chell_io) where
-	chell_io opts = do
-		let seed = Chell.testOptionSeed opts
-		
-		term <- Text.newNullTerminal
-		
-		let args = QuickCheck.stdArgs
-		let state = State.MkState
-			{ State.terminal = term
-			, State.maxSuccessTests = QuickCheck.maxSuccess args
-			, State.maxDiscardedTests = QuickCheck.maxDiscard args
-			, State.computeSize = computeSize
-			  	(QuickCheck.maxSize args)
-			  	(QuickCheck.maxSuccess args)
-			, State.numSuccessTests = 0
-			, State.numDiscardedTests = 0
-			, State.collected = []
-			, State.expectedFailure = False
-			, State.randomSeed = mkStdGen seed
-			, State.numSuccessShrinks = 0
-			, State.numTryShrinks = 0
-			}
-		
-		result <- Test.test state (Gen.unGen (QuickCheck.property prop))
-		let output = pack (Test.output result)
-		return $ case result of
-			Test.Success{} -> Chell.TestPassed
-				[("seed", pack (show seed))]
-			Test.Failure{} -> Chell.TestFailed
-				[("seed", pack (show seed))]
-				[Chell.Failure Nothing output]
-			Test.GaveUp{} -> Chell.TestAborted
-				[("seed", pack (show seed))]
-				output
-			Test.NoExpectedFailure{} -> Chell.TestFailed
-				[("seed", pack (show seed))]
-				[Chell.Failure Nothing output]
+property :: QuickCheck.Testable prop => String -> prop -> Chell.Test
+property name prop = Chell.test name $ \opts -> do
+	let seed = Chell.testOptionSeed opts
+	
+	term <- Text.newNullTerminal
+	
+	let args = QuickCheck.stdArgs
+	let state = State.MkState
+		{ State.terminal = term
+		, State.maxSuccessTests = QuickCheck.maxSuccess args
+		, State.maxDiscardedTests = QuickCheck.maxDiscard args
+		, State.computeSize = computeSize (QuickCheck.maxSize args) (QuickCheck.maxSuccess args)
+		, State.numSuccessTests = 0
+		, State.numDiscardedTests = 0
+		, State.collected = []
+		, State.expectedFailure = False
+		, State.randomSeed = mkStdGen seed
+		, State.numSuccessShrinks = 0
+		, State.numTryShrinks = 0
+		}
+	
+	result <- Test.test state (Gen.unGen (QuickCheck.property prop))
+	let output = Test.output result
+	let notes = [("seed", show seed)]
+	let failure = Chell.failure { Chell.failureMessage = output }
+	return $ case result of
+		Test.Success{} -> Chell.TestPassed notes
+		Test.Failure{} -> Chell.TestFailed notes [failure]
+		Test.GaveUp{} -> Chell.TestAborted notes output
+		Test.NoExpectedFailure{} -> Chell.TestFailed notes [failure]
 
 -- copied from quickcheck-2.4.1.1/src/Test/QuickCheck/Test.hs
 computeSize :: Int -> Int -> Int -> Int -> Int
